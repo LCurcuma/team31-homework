79464634285235e64faf48e9350d3773bdaaa7a5
LCurcuma <zhihnadya2002@gmail.com>
2025-02-07 19:00:44 +0100

    deleted comments and edited html and css files a little

--- javascript/javascript1/week1/nadiia/Goodboy-Oldboy.js
+++ javascript/javascript1/week1/nadiia/Goodboy-Oldboy.js
@@ -20,24 +20,3 @@ function dogAgeIfy(){
 }
 
 }
-
-/*function dogAgeIfyInHuman(){
-
-    let dogYearOfBirth = parseInt(document.getElementById("dogYearOfBirth").value, 10);
-    let dogYearFuture = parseInt(document.getElementById("dogsFutureYear").value, 10);
-    let dogYear = dogYearFuture - dogYearOfBirth;
-
-    document.getElementById("dogFutureAge").innerText = "Your dog will be " + dogYear + " human years old in " + dogYearFuture + ".";
-}*/
-
-/* With if/else statement:
-let shouldShowResultInDogYears = true; //or false
-
-if(shouldShowResultInDogYears){
-dogYear = (dogYearFuture - dogYearOfBirth) * 7;
-console.log("Your dog will be", dogYear, "dog years old in", dogYearFuture);
-}
-else{
-dogYear = dogYearFuture - dogYearOfBirth;
-console.log("Your dog will be", dogYear, "human years old in", dogYearFuture);
-}*/
