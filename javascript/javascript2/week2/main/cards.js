da4728acb55e4fef7f8fa27c4368c652bd24b5f4
LCurcuma <zhihnadya2002@gmail.com>
2025-03-17 20:42:16 +0100

    added all cards and edited files

--- javascript/javascript2/week2/main/cards.js
+++ javascript/javascript2/week2/main/cards.js
@@ -32,4 +32,39 @@ const cards = [
     name: "Lin",
     url: "card_lin.png",
   },
+  {
+    id: "remi",
+    name: "Remi",
+    url: "card_remi.png",
+  },
+  {
+    id: 'mortis',
+    name: 'Mortis',
+    url: 'card_mortis.png'
+  },
+  {
+    id: 'rena',
+    name: 'Rena',
+    url: 'card_rena.png'
+  },
+  {
+    id: 'madlen',
+    name: 'Madlen',
+    url: 'card_madlen.png'
+  },
+  {
+    id: 'kallas',
+    name: 'Kallas',
+    url: 'card_kallas.png'
+  },
+  {
+    id: 'richi',
+    name: 'Richi',
+    url: 'card_richi.png'
+  },
+  {
+    id: 'linda',
+    name: 'Linda',
+    url: 'card_linda.png'
+  }
 ];
