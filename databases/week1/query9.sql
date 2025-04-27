4fb9bf2a82901d40a3545652b254348c774c1df1
Nadiia <zhihnadya2002@gmail.com>
2025-04-27 16:13:03 +0200

    Update query9.sql

--- databases/week1/query9.sql
+++ databases/week1/query9.sql
@@ -1,13 +1,9 @@
 USE lesson1;
 
-SELECT 'Not started' AS status, COUNT(*) AS amount_of_tasks
+SELECT 
+	status.name AS status_name,
+	COUNT(task.status_id) AS amount_of_tasks
 FROM task
-WHERE status_id = 1
-UNION
-SELECT 'In progress' AS status, COUNT(*) AS amount_of_tasks
-FROM task
-WHERE status_id = 2
-UNION
-SELECT 'Done' AS status, COUNT(*) AS amount_of_tasks
-FROM task
-WHERE status_id = 3
+JOIN status
+ON status.id = task.status_id
+GROUP BY status.id
