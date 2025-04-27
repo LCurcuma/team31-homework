be7e0b9847582807f0298225fa7a4cffed7cb81b
Nadiia <zhihnadya2002@gmail.com>
2025-04-27 16:13:24 +0200

    Update query10.sql

--- databases/week1/query10.sql
+++ databases/week1/query10.sql
@@ -1,17 +1,10 @@
 USE lesson1;
 
-SELECT status.id, status.name, x.amount_of_tasks
-FROM status
-LEFT JOIN (
-SELECT 1 AS status_id, COUNT(*) AS amount_of_tasks
+SELECT 
+	status.name AS status_name,
+	COUNT(task.status_id) AS amount_of_tasks
 FROM task
-WHERE status_id = 1
-UNION
-SELECT 2 AS status_id, COUNT(*) AS amount_of_tasks
-FROM task
-WHERE status_id = 2
-UNION
-SELECT 3 AS status_id, COUNT(*) AS amount_of_tasks
-FROM task
-WHERE status_id = 3
-) x ON x.status_id = status.id
+JOIN status
+ON status.id = task.status_id
+GROUP BY status.id
+ORDER BY amount_of_tasks DESC
